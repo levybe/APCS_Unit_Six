aa0ebdfbfe4ab9cd0657855666507cbd3c1437b6
levybe <levybe@ssfs.org>
2021-12-10 14:29:27 -0500

    Completed SpellChecker.

--- src/SpellChecker.java
+++ src/SpellChecker.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class SpellChecker {
-    public static String[] getDictionary() throws IOException {
+    public static String[] getDictionary() throws IOException { // Collects the words inside the "words" text file and puts them into an array.
         String[] dictionary = new String[61336];
         Scanner wordsScan = new Scanner(new File("files/words.txt"));
         int count = 0;
@@ -35,16 +35,49 @@ public class SpellChecker {
         return false;
     }
 
-    public static void write(String[] arr) throws IOException {
-        FileWriter fw = new FileWriter("files/writeto.txt");
-        for (String word : arr) {
-            fw.write(word + " ");
-        }
-        fw.close();
-    }
-
     public static void main (String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
         Scanner textScan = new Scanner(new File("files/alice.txt"));
         FileWriter fw = new FileWriter("files/corrected.txt");
+
+        String[] dictionary = getDictionary();
+        String confirm = "";
+        boolean answered = false;
+        String newWord = "";
+        String word = "";
+
+        System.out.println("Checking the text file \"alice.txt\" for typos using words provided by \"words.txt\"...");
+        while (textScan.hasNext()) {
+            word = textScan.next();
+            if (wordInDictionary(word, dictionary)) {
+                fw.write(word + " ");
+            }
+            else {
+                System.out.println("");
+                System.out.println("The word \"" + word + "\" is not found within the provided dictionary.\nWould you like to rewrite the word or keep it as is?\nType \"Y\" to rewrite or \"N\" to keep.");
+                confirm = scan.nextLine();
+                while (!answered) {
+                    if (confirm.equals("Y") || confirm.equals("y")) {
+                        System.out.println("Please enter the rewritten word.");
+                        newWord = scan.nextLine();
+                        fw.write(newWord + " ");
+                        System.out.println("The word \"" + word + "\" has been replaced with \"" + newWord + "\".");
+                        answered = true;
+                    }
+                    else if (confirm.equals("N") || confirm.equals("n")) {
+                        fw.write(word + " ");
+                        System.out.println("The word \"" + word + "\" has not been replaced.");
+                        answered = true;
+                    }
+                    else {
+                        System.out.println("Please enter a valid answer.");
+                    }
+                }
+                answered = false;
+            }
+        }
+        fw.close();
+        System.out.println("");
+        System.out.println("The text file \"alice.txt\" has been checked for typos.\nThe corrected text can be found in the file \"corrected.txt\".");
     }
 }
